13aef22208118de523c79682c0728e42fd3855cd
Will Sutton <sutton_999@hotmail.com>
2020-11-26 18:28:40 +0000

    Update Chapter 4 Lab - Logistic Regression.R

--- Chapter 4 Lab - Logistic Regression.R
+++ Chapter 4 Lab - Logistic Regression.R
@@ -145,7 +145,76 @@ mean(knn.pred == Direction.2005)
 
 
 ### An Application to Caravan Insurance Data
-# To Do
+
+library(ISLR)
+library(class)
+
+dim(Caravan)
+attach(Caravan)
+# Purchased caravan insurance
+summary(Purchase)
+348/5822
+
+# standardizing variables for KNN
+# standard deviation of one 
+# and a mean of zero
+standardized.X <- scale(Caravan[,-86])
+var(Caravan[,1])
+var(Caravan[,2])
+
+var(standardized.X[,1])
+var(standardized.X[,2])
+
+# define train and test datasets
+test <- 1:1000
+train.X <- standardized.X[-test,]
+test.X <- standardized.X[test,]
+
+train.Y <- Purchase[-test]
+test.Y <- Purchase[test]
+
+set.seed(1)
+knn.pred <- knn(train.X,test.X,train.Y,k=1)
+mean(test.Y!=knn.pred)
+mean(test.Y!='No')
+
+# KNN error rate is 12%, actual is 6%
+
+table(knn.pred,test.Y)
+9/(68+9)
+
+# k = 3
+knn.pred <- knn(train.X,test.X,train.Y,k=3)
+table(knn.pred,test.Y)
+5/26
+
+# k = 5
+knn.pred <- knn(train.X,test.X,train.Y,k=5)
+table(knn.pred,test.Y)
+4/15
+
+# Compare to logistic regression model
+
+glm.fits <- glm(Purchase~.
+                ,data = Caravan
+                ,family = binomial
+                ,subset = -test)
+
+glm.probs <- predict(glm.fits, Caravan[test,], type = "response")
+glm.pred <- rep("No",1000)
+glm.pred[glm.probs>.5] <- "Yes"
+
+table(glm.pred,test.Y)
+
+glm.pred <- rep("No",1000)
+glm.pred[glm.probs>.25] <- "Yes"
+
+table(glm.pred,test.Y)
+11/33
+
+
+
+
 
 
 
